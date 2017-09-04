744aac015ffa367c5e1cef5308760ac3b2932441
Kenneth Crawford <kmc059000@gmail.com>
2017-09-04 13:25:44 -0500

    added tests project

---
+++ src/MerkleTree.Tests/MerkleTreeTests.fs
@@ -0,0 +1,18 @@
+﻿module MerkleTreeTests
+
+open Expecto
+open MerkleTree
+
+[<Tests>]
+let tests = 
+    testList "main" [
+        test "Hello World" {
+            Expect.equal 1 1 "1 != 2"
+        }
+    ]
+
+[<EntryPoint>]
+let main args = 
+    let result = runTestsInAssembly defaultConfig args
+    System.Console.ReadLine() |> ignore
+    result
